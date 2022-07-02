f2019ce3efe718d32a6b928e047164c90ca6b6be
triszt4n <trisztanpiller@gmail.com>
2022-07-03 01:04:18 +0200

    Things started to work!

--- src/main/client/src/components/@pages/GroupSelectionPage.tsx
+++ src/main/client/src/components/@pages/GroupSelectionPage.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { Page } from '../@layout/Page'
-import { Alert, AlertIcon, Button, ButtonGroup, FormControl, FormLabel, Heading, Select, Text, VStack } from '@chakra-ui/react'
-import { Helmet } from 'react-helmet'
-import { LinkButton } from '../@commons/LinkButton'
+import { Button, ButtonGroup, FormControl, FormLabel, Heading, Select, Text, VStack } from '@chakra-ui/react'
 import axios from 'axios'
-import { API_BASE_URL } from '../../utils/configurations'
-import { GroupChangeDTO, GroupChangeStatus } from '../../types/dto/groupChange'
-import { useServiceContext } from '../../utils/useServiceContext'
+import React, { useEffect, useState } from 'react'
+import { Helmet } from 'react-helmet'
 import { Navigate, useNavigate } from 'react-router-dom'
+import { GroupChangeDTO, GroupChangeStatus } from '../../types/dto/groupChange'
+import { API_BASE_URL } from '../../utils/configurations'
 import { useAuthContext } from '../../utils/useAuthContext'
+import { useServiceContext } from '../../utils/useServiceContext'
+import { LinkButton } from '../@commons/LinkButton'
+import { Page } from '../@layout/Page'
 
 export const GroupSelectionPage: React.FC = () => {
   const { profile, updateProfile } = useAuthContext()
@@ -33,7 +33,7 @@ export const GroupSelectionPage: React.FC = () => {
               navigate('/profil')
               break
             case GroupChangeStatus.INVALID_GROUP:
-              setError('Érvénytelen tankör!')
+              setError('Érvénytelen kör!')
               break
             case GroupChangeStatus.LEAVE_DENIED:
               setError('Nem engedélyezett a módosítás!')
@@ -49,31 +49,25 @@ export const GroupSelectionPage: React.FC = () => {
         .catch(() => {
           throwError('Nem sikerült a mentés.', { toast: true })
         })
-    else setError('Válassz tankört!')
+    else setError('Válassz kört!')
   }
 
   if (!profile || !profile.groupSelectionAllowed) return <Navigate to="/profil" />
 
   return (
     <Page>
-      <Helmet title="Tankör beállítása" />
-      <Heading align="center">Tankör beállítása</Heading>
+      <Helmet title="Kör beállítása" />
+      <Heading align="center">Kör beállítása</Heading>
       <Text mt={10} textAlign="center">
-        Állítsd be a tankörödet, hogy részt vehess a feladatokban!
+        Állítsd be a körödet, hogy részt vehess a feladatokban!
       </Text>
       <Text color="gray.500" textAlign="center">
-        Csak helyesen beállított tankörrel fog érvényesülni a tanköri jelenlét.
+        A Vendég jelentése: egyik körbe sem tartozol.
       </Text>
-      <Alert status="warning" mt={10} variant="left-accent">
-        <AlertIcon />A tanköröd módosítása után már nem tudod újra megváltoztatni.
-      </Alert>
-      <Alert status="info" mt={5} variant="left-accent">
-        <AlertIcon />A VENDÉG jelentése: egyik tankörbe sem tartozol, ezért a tanköri jelenlétet sem fogják majd jóváírni!
-      </Alert>
       <form>
         <VStack spacing={5} mt={10} maxW={80} mx="auto">
           <FormControl>
-            <FormLabel>Melyik tankörbe tartozol?</FormLabel>
+            <FormLabel>Melyik körbe tartozol?</FormLabel>
             <Select
               id="group"
               value={value}
@@ -88,24 +82,13 @@ export const GroupSelectionPage: React.FC = () => {
               ))}
             </Select>
           </FormControl>
-          <ButtonGroup>
-            <Button
-              variant="ghost"
-              colorScheme="brand"
-              onClick={() => {
-                setValue(profile?.fallbackGroup.toString())
-              }}
-            >
-              Vendég kiválasztása
-            </Button>
-          </ButtonGroup>
-          <ButtonGroup>
-            <Button onClick={onSubmit} colorScheme="brand">
-              Mentés
-            </Button>
+          <ButtonGroup spacing={4}>
             <LinkButton href="/profil" colorScheme="red" variant="outline">
               Mégse
             </LinkButton>
+            <Button onClick={onSubmit} colorScheme="brand">
+              Mentés
+            </Button>
           </ButtonGroup>
           {error && (
             <Text color="red.500" textAlign="center">
